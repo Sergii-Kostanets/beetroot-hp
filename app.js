713b6c2816115708766adbf3ac9db3fad4025c80
Sergii-Kostanets <sergii.kostanets@ua.nestle.com>
2022-02-10 18:08:51 +0200

    some changes at app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-document.write("Привет от JS из отдельного файла app.js")
+document.write('Привет от JS из отдельного файла app.js<br>')
+var firstName = 'Sergii'
+const lastName = 'Kostanets'
+let age = 31
+firstName = 'Serg<br>'
+document.write(firstName)
+console.log(lastName)
+age = 32
+document.write(age + '-летний<br>')
+const mood = prompt('Как настроение?')
+document.write('Настроение ' + mood)
